3e7f3a268d7dca4f2e41ae934269e925f495c231
Filip Berntsson <filbern@alvis1.int.private>
2025-09-08 15:32:57 +0200

    updated __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -3,7 +3,7 @@ Training module for fungal classification
 """
 
 from .data import load_fold_data, prepare_data_for_training, create_data_loaders
-from .preprocessing import  KmerTokenizer
+from .preprocessing import  KmerTokenizer, augment_sequence
 from .model import create_model, SequenceClassificationModel
 from .trainer import Trainer
 
@@ -12,6 +12,7 @@ __all__ = [
     'prepare_data_for_training',
     'create_data_loaders',
     'KmerTokenizer',
+    'augment_sequence',
     'create_model',
     'SequenceClassificationModel',
     'Trainer'
